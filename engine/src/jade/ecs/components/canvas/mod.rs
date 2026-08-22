90532ea05226cd419b19348c956825235a57bcda
Sophie Holman <xiphias.xiphian@gmail.com>
2026-08-22 21:11:05 +0100

    basic canvas component

---
+++ engine/src/jade/ecs/components/canvas/mod.rs
@@ -0,0 +1,15 @@
+use crate::{jade::ecs::component::Component, renderer::primitive::draw_command::DrawCommand};
+
+pub struct Canvas
+{
+    draw_commands: Vec<DrawCommand>
+}
+
+impl Canvas
+{
+    pub fn commands(&self) -> &[DrawCommand] { &self.draw_commands }
+}
+
+impl Component for Canvas
+{
+}
